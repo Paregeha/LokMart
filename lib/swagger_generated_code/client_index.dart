b30f660ce09eafd29aab1c235494354303827192
Oleksandr Perehinchuk <paregeha@gmail.com>
2025-11-10 13:52:17 +0200

    commit

--- lib/swagger_generated_code/client_index.dart
+++
@@ -1,4 +0,0 @@
-export 'full_documentation.swagger.dart' show FullDocumentation;
-export 'full_documentation.swagger.dart' show FullDocumentation;
-export 'some_file_name.swagger.dart' show SomeFileName;
-export 'some_file_name.swagger.dart' show SomeFileName;
